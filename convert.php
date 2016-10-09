6928e46c9cb94546567f1d9cb7492563f97677d7
Mohsin Rasool <mohsin.rasool@gmail.com>
2016-10-10 00:12:59 +0500

    conversion in process

--- convert.php
+++ convert.php
@@ -1,6 +1,7 @@
 <?php
 
 include 'vendor/autoload.php';
+include 'db_layer.php';
 
 use Intercom\IntercomClient;
 
@@ -33,11 +34,86 @@ class BodyRockLeadConversion
 
 	}
 
-	public function run($value='')
+	public function fetchAndConvert()
 	{
-		print_r($this->client->leads->getLeads([]));
-		// $users = $this->client->users->getUsers([]);
-		// print_r($users);
+		$db = new Database('localhost','root','','bodyrock_intercom');
+
+		$totalPages = null;
+		$currentPage = 1;
+		// $leads = $this->client->leads->getLeads(['created_since'=>'2']);
+		// $leads = $this->client->leads->getLeads(['segment_id'=>'57f7c6d08bc828fa6fe2a963']);
+		$leads = $this->client->leads->getLeads(['email' => 'nbuckram@gmail.com']);
+		print_r($leads);
+
+
+		foreach ($leads->contacts as $lead) {
+			if(empty($lead->email))
+				continue;
+
+			$record = array(
+				'id' => $lead->id,
+				'user_id' => $lead->user_id,
+				'email' => $lead->email,
+				'name' => $lead->name,
+				'created_at' => $lead->created_at,
+				'anonymous' => $lead->anonymous,
+				);
+			$db->saveRecord($record, 'cron_log');
+		}
+	}
+
+	public function convertFromDB()
+	{
+		$db = new Database('localhost','root','','bodyrock_intercom');
+
+		$dbLeads = $db->getResults('*','leads', array('contact_id'=>''));
+
+		if(!$dbLeads) {
+			echo 'No lead available to convert.';
+		}
+
+		for ($i=0; $i < $db->numRecords($dbLeads); $i++) {
+
+			$contact = null;
+			$dbLead = $db->getObject($dbLeads);
+
+			// $leads = $this->client->leads->getLeads(['email'=>$dbLead->email]);
+
+			// if(empty($leads->contacts))
+			// 	continue;
+
+			echo "<h1>Leads</h1>";
+			print_r($dbLead);
+			// foreach($leads->contacts as $lead) {
+
+				// if($contact == null)
+					$contact = $this->client->users->getUsers(['email'=>$dbLead->email]);
+
+				echo "<h1>Contact</h1>";
+				print_r($contact);
+
+				$convertData = array("contact" => array("user_id" => $dbLead->user_id));
+				if(!empty($contact))
+					$convertData['user'] = array("user_id" => $contact->user_id);
+
+				echo "<h1>Convert Data</h1>";
+				print_r($convertData);
+				try {
+
+					// $response = $this->client->leads->convertLead($convertData);
+
+					// if(!empty($response->id)) {
+					// 	$db->query("update leads set contact_id='".$response->id."' where id='".$dbLead->id."'; ");
+					// }
+
+				}catch(\Exception $ex) {
+
+				}
+				echo "<h1>Conversion Response</h1>";
+				// print_r($response);
+				exit;
+			// }
+		}
 	}
 
 } // END BodyRockConversion class
@@ -45,6 +121,6 @@ class BodyRockLeadConversion
 
 
 $conv = new BodyRockLeadConversion();
-$conv->run();
+$conv->convertFromDB();
 
 ?>
